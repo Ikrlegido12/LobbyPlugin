814698576e48aa551ae7f03d87687b8400f34c92
IkrlegidO <Ikermardi2010huawei@gmail.com>
2024-09-08 16:09:21 +0200

    teest

--- src/main/java/bh/bahiaminecraft/lobbyPlugin/LobbyPlugin.java
+++ src/main/java/bh/bahiaminecraft/lobbyPlugin/LobbyPlugin.java
@@ -1,12 +1,15 @@
 package bh.bahiaminecraft.lobbyPlugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LobbyPlugin extends JavaPlugin {
-
+    public final String prefix = "&7[&bBahiaMinecraft&7] ";
+    public String version = getDescription().getVersion();
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getConsoleSender(ChatColor.translateAlternateColorCodes('&',prefix+"&ePlugin iniciado correctamente, version:&3 "));
 
     }
 
